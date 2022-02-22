d8abb0724841f94cf8e9ac15bf8afeccf6800df4
jeonghs1024 <jeonghs1024@gmail.com>
2022-02-22 00:34:09 -0600

    index.html appearance

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  res.send("test");
+    res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
 app.listen(port, () =>

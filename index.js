4479732963162ad91885d35bf33ab1c6b72b4d11
StaishaKnight <staishaknight@gmail.com>
2022-02-22 12:39:03 -0600

    added blog section

--- index.js
+++ index.js
@@ -12,3 +12,50 @@ app.get("/", (req, res) => {
 app.listen(port, () =>
   console.log("Hello world app listening on port ${port}!")
 );
+
+(function(){
+  var app = angular.module('blogApp',[]);
+  
+  app.controller('BlogController', ['$http', function($http){
+    
+    var blog = this;
+    blog.title = "AngularJS Blog App";
+    
+    blog.posts = {};
+    $http.get('https://s3-us-west-2.amazonaws.com/s.cdpn.io/110131/posts_1.json').success(function(data){
+      blog.posts = data;
+    });
+    
+    blog.tab = 'blog';
+    
+    blog.selectTab = function(setTab){
+      blog.tab = setTab;
+      console.log(blog.tab)
+    };
+    
+    blog.isSelected = function(checkTab){
+      return blog.tab === checkTab;
+    };
+    
+    blog.post = {};
+    blog.addPost = function(){
+      blog.post.createdOn = Date.now();
+      blog.post.comments = [];
+      blog.post.likes = 0;
+      blog.posts.unshift(this.post);
+      blog.tab = 0;
+      blog.post ={};
+    };   
+    
+  }]);
+  
+  app.controller('CommentController', function(){
+    this.comment = {};
+    this.addComment = function(post){
+      this.comment.createdOn = Date.now();
+      post.comments.push(this.comment);
+      this.comment ={};
+    };
+  });
+ 
+})();

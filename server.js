d3cae57cf87cc32b7e1fa3103597814c9c8a492b
loch <nigelloch@gmail.com>
2022-02-22 20:06:11 -0600

    merge to develop

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-<<<<<<< HEAD
 const sequelize = require('./config/connection');
-=======
-const sequelize = require("./config/connection");
->>>>>>> bae5b7774b013495fb0774e258d4b40104ad5a37
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -34,15 +30,9 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, 'public/')));
 
 app.use(require('./controllers'));
-=======
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(require('./controllers/'));
->>>>>>> bae5b7774b013495fb0774e258d4b40104ad5a37
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));

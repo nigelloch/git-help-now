38c6a67b29fcd8442bb102219317280280ee4901
Chris5801 <91812299+Chris5801@users.noreply.github.com>
2022-02-23 20:18:30 -0600

    Revert "Logins"

--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router()
-const homeroutes = require("./homeroutes")
-router.use("/",homeroutes)
-
-module.exports = router
--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS git_help_db;
-
-CREATE DATABASE git_help_db;

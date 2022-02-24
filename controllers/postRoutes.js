84d756422dd9b4a83e20a772b23d90cf7d03fa01
Johnhughes814 <8hughes.john@gmail.com>
2022-02-23 19:10:06 -0600

    created ui handlebar files, routes and javascript

---
+++ controllers/postRoutes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+const Comment = require("../models/Comment");
+
+//on post page load render post data
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Comment,
+          //join of joined data
+          include: [
+            {
+              model: User,
+              attributes: ["id", "user_id"],
+            },
+          ],
+        },
+        {
+          model: User,
+          attributes: ["id", "user_id"],
+        },
+      ],
+    });
+    const post = postData.get({ plain: true });
+    console.log(post);
+    if (post) {
+      res.render("post", {
+        loggedIn: req.session.loggedIn,
+        loggedInUserData: req.session.loggedInUserData,
+        postData: post,
+      });
+    } else {
+      res.redirect("/");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
